f2d263a90ccb0c8090790004905bcb9a92d1255f
정채은 <cce523@hanmail.net>
2023-06-26 13:20:16 +0900

    [Chore] #2 스토리보드 삭제

---
+++ YELLO-iOS/YELLO-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YELLO-iOS
+//
+//  Created by 정채은 on 2023/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
