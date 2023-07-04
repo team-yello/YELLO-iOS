afa4b306f2fef89adde1d5d70c7eb2ace13dcfb9
Zoe <68178395+Zoe0929@users.noreply.github.com>
2023-07-04 23:25:59 +0900

    Revert "Chore [#6] 디자인 시스템 구축"

--- YELLO-iOS/YELLO-iOS/Global/Extensions/UILabel+.swift
+++ YELLO-iOS/YELLO-iOS/Global/Extensions/UILabel+.swift
@@ -8,22 +8,5 @@
 import UIKit
 
 extension UILabel {
-    func setTextWithLineHeight(text: String?, lineHeight: CGFloat) {
-        if let text = text {
-            let style = NSMutableParagraphStyle()
-            style.maximumLineHeight = lineHeight
-            style.minimumLineHeight = lineHeight
-            
-            let attributes: [NSAttributedString.Key: Any] = [
-                .paragraphStyle: style,
-                .baselineOffset: (lineHeight - font.lineHeight) * 0.26
-            ]
-            
-            let attrString = NSAttributedString(string: text,
-                                                attributes: attributes)
-            self.attributedText = attrString
-            self.textAlignment = .center
-            self.numberOfLines = 2
-        }
-    }
+
 }
