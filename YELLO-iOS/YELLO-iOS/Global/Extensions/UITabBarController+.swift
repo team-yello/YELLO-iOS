16b49bc699539d3fe68275a722f387a5b173ce8e
heejoo <heejoo_byun@naver.com>
2023-07-06 01:51:17 +0900

    Add [#10] TabBar - UITabBarController+ extension 추가

---
+++ YELLO-iOS/YELLO-iOS/Global/Extensions/UITabBarController+.swift
@@ -0,0 +1,33 @@
+//
+//  UITabBarController+.swift
+//  YELLO-iOS
+//
+//  Created by 변희주 on 2023/07/06.
+//
+
+import UIKit
+
+extension CALayer {
+    // Sketch 스타일의 그림자를 생성하는 유틸리티 함수
+    func applyShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4
+    ) {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+    }
+}
+
+extension UITabBar {
+    // 기본 그림자 스타일을 초기화해야 커스텀 스타일을 적용할 수 있다.
+    static func clearShadow() {
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+        UITabBar.appearance().backgroundColor = UIColor.white
+    }
+}
