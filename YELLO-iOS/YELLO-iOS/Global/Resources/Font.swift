6936465ec8fb375d65c5e7f0202c0840683ca5eb
정채은 <cce523@hanmail.net>
2023-07-07 00:03:33 +0900

    Fix [#13] font 및 color 수정

--- YELLO-iOS/YELLO-iOS/Global/Resources/Font.swift
+++ YELLO-iOS/YELLO-iOS/Global/Resources/Font.swift
@@ -5,72 +5,63 @@
 //  Created by 정채은 on 2023/06/26.
 //
 
-
 import UIKit
 
 extension UIFont {
-    
-    static var uiHeadline00: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 22.0)!
-    }
-    
-    static var uiHeadline01: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 21.0)!
-    }
-    
-    static var uiHeadline02: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 20.0)!
-    }
-    
-    static var uiSubtitle01: UIFont {
-        return UIFont(name: "Pretendard-SemiBold", size: 18.0)!
-    }
-    
-    static var uiSubtitle02: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 16.0)!
-    }
-    
-    static var uiBody01: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 16.0)!
-    }
-    
-    static var uiBtn01: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 15.0)!
-    }
-    
-    static var uiBody02: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 15.0)!
-    }
-    
-    static var uiBody03: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 14.0)!
-    }
-    
-    static var uiBtn02: UIFont {
-        return UIFont(name: "Pretendard-SemiBold", size: 13.0)!
-    }
-    
-    static var uiBody04: UIFont {
-        return UIFont(name: "Pretendard-Regular", size: 13.0)!
-    }
-    
-    static var uiCaption01: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 12.0)!
-    }
-    
-    static var uiCaption02: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 11.0)!
-    }
-    
-    static var uiCaption03: UIFont {
-        return UIFont(name: "Pretendard-Medium", size: 10.0)!
-    }
-    
-    static var uiCaption04: UIFont {
-        return UIFont(name: "Pretendard-Bold", size: 10.0)!
-    }
-    
-    static var Graphic: UIFont {
-        return UIFont(name: "Unbounded-Bold", size: 32.0)!
-    }
+
+  class var uiHeadline01: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 22.0)!
+  }
+
+  class var uiHeadline02: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 21.0)!
+  }
+
+  class var uiHeadline03: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 20.0)!
+  }
+
+  class var uiSubtitle01: UIFont {
+    return UIFont(name: "Pretendard-SemiBold", size: 18.0)!
+  }
+
+  class var uiSubtitle02: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 16.0)!
+  }
+
+  class var uiBodyLarge: UIFont {
+    return UIFont(name: "Pretendard-Medium", size: 16.0)!
+  }
+
+  class var uiBody01: UIFont {
+    return UIFont(name: "Pretendard-Medium", size: 15.0)!
+  }
+
+  class var uiBodyMedium: UIFont {
+    return UIFont(name: "Pretendard-Medium", size: 15.0)!
+  }
+
+  class var uiBodySmall: UIFont {
+    return UIFont(name: "Pretendard-Medium", size: 14.0)!
+  }
+
+  class var uiButton: UIFont {
+    return UIFont(name: "Pretendard-SemiBold", size: 13.0)!
+  }
+
+  class var uiBody02: UIFont {
+    return UIFont(name: "Pretendard-Regular", size: 13.0)!
+  }
+
+  class var uiLabelLarge: UIFont {
+    return UIFont(name: "Pretendard-Medium", size: 12.0)!
+  }
+
+  class var uiLabelMedium: UIFont {
+    return UIFont(name: "Pretendard-Medium", size: 11.0)!
+  }
+
+  class var uiLabelSmall: UIFont {
+    return UIFont(name: "Pretendard-Medium", size: 10.0)!
+  }
 }
