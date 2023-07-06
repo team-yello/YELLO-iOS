4de02578d971b6ccf7c5eeeac9c3954014200a41
정채은 <cce523@hanmail.net>
2023-07-07 00:07:56 +0900

    Feat [#13] 둘러보기뷰 구현

--- YELLO-iOS/YELLO-iOS/Presentation/Around/AroundViewController.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Around/AroundViewController.swift
@@ -2,28 +2,41 @@
 //  AroundViewController.swift
 //  YELLO-iOS
 //
-//  Created by 변희주 on 2023/07/05.
+//  Created by 정채은 on 2023/07/05.
 //
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class AroundViewController: UIViewController {
+
+    private let aroundView = AroundView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension AroundViewController {
+    
+    private func setUI() {
+        setStyle()
+        setLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setStyle() {
+        view.backgroundColor = .black
     }
-    */
-
+    
+    private func setLayout() {
+        view.addSubviews(aroundView)
+        
+        aroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+    }
 }
