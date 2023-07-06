a545d7fe19c3a063c5326829ffc58a9cd0a551b4
Zoe <Zoe.urssu@gmail.com>
2023-07-06 23:57:32 +0900

    Add [#8] 오토레이아웃용 변수 저장

---
+++ YELLO-iOS/YELLO-iOS/Presentation/Onboarding/Constraints.swift
@@ -0,0 +1,15 @@
+//
+//  Constraints.swift
+//  YELLO-iOS
+//
+//  Created by 지희의 MAC on 2023/07/06.
+//
+
+import Foundation
+
+class Constraints {
+    static var bigMargin = 16
+    static var smallMargin = 12
+    static var topMargin = 20
+    static var bottomMargin = 34
+}
