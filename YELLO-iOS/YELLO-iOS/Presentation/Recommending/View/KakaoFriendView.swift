16505fd1227f23885e3f2a1f13c3556ae4c3dd34
정채은 <cce523@hanmail.net>
2023-07-10 18:33:01 +0900

    Chore [#21] 폴더링, 파일 및 에셋 추가

---
+++ YELLO-iOS/YELLO-iOS/Presentation/Recommending/View/KakaoFriendView.swift
@@ -0,0 +1,20 @@
+//
+//  KakaoFriendView.swift
+//  YELLO-iOS
+//
+//  Created by 정채은 on 2023/07/10.
+//
+
+import UIKit
+
+class KakaoFriendView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
