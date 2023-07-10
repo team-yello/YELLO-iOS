e0b64e441030e4273f5ed7452bf6afa5ed74a3cb
정채은 <cce523@hanmail.net>
2023-07-10 19:47:59 +0900

    Add [#21] 파일 추가

---
+++ YELLO-iOS/YELLO-iOS/Presentation/Recommending/View/EmptyFriendView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyView.swift
+//  YELLO-iOS
+//
+//  Created by 정채은 on 2023/07/10.
+//
+
+import UIKit
+
+class EmptyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
