57dfacb9e139d98ac7e98ca65ef9428d855029c4
정채은 <cce523@hanmail.net>
2023-07-12 16:58:36 +0900

    Style [#28] 내 옐로 테이블 뷰 기본 Setting

---
+++ YELLO-iOS/YELLO-iOS/Presentation/MyYello/Cells/MyYelloKeywordTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MyYelloKeywordTableViewCell.swift
+//  YELLO-iOS
+//
+//  Created by 정채은 on 2023/07/12.
+//
+
+import UIKit
+
+class MyYelloKeywordTableViewCell: UITableViewCell {
+    
+    static let identifier = "MyYelloKeywordTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setStyle()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setStyle() {
+        
+    }
+    
+    private func setLayout() {
+        
+    }
+}
---
+++ YELLO-iOS/YELLO-iOS/Presentation/MyYello/View/MyYelloListView.swift
@@ -0,0 +1,51 @@
+//
+//  MyYelloListView.swift
+//  YELLO-iOS
+//
+//  Created by 정채은 on 2023/07/12.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class MyYelloListView: BaseView {
+    
+    lazy var myYelloTableView = UITableView()
+    
+    override func setStyle() {
+        self.backgroundColor = .black
+        
+        myYelloTableView.do {
+            $0.register(MyYelloDefaultTableViewCell.self, forCellReuseIdentifier: MyYelloDefaultTableViewCell.identifier)
+            $0.register(MyYelloKeywordTableViewCell.self, forCellReuseIdentifier: MyYelloKeywordTableViewCell.identifier)
+            $0.register(MyYelloNameTableViewCell.self, forCellReuseIdentifier: MyYelloNameTableViewCell.identifier)
+            $0.dataSource = self
+            $0.delegate = self
+            $0.separatorStyle = .none
+            $0.showsVerticalScrollIndicator = false
+            $0.showsHorizontalScrollIndicator = false
+        }
+    }
+    
+    override func setLayout() {
+        self.addSubviews(myYelloTableView)
+        
+        myYelloTableView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+    }
+}
+
+extension MyYelloListView: UITableViewDelegate { }
+extension MyYelloListView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
