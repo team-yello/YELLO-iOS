b2a01beeb8867108968f6e6595962d4a612d8757
Zoe <Zoe.urssu@gmail.com>
2023-07-15 01:54:51 +0900

    Style [#49] 카카오로그인 뷰 구현

--- YELLO-iOS/YELLO-iOS/Presentation/Onboarding/ViewController/KakaoLoginViewController.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Onboarding/ViewController/KakaoLoginViewController.swift
@@ -7,23 +7,25 @@
 
 import UIKit
 
-class KakaoLoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class KakaoLoginViewController: BaseViewController {
+    
+    let baseView = KakaoLoginView()
+    
+    override func loadView() {
+        view = baseView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addTarget()
     }
-    */
 
+    func addTarget() {
+        baseView.kakaoButton.addTarget(self, action: #selector(kakaoLoginButtonDidTap), for: .touchUpInside)
+    }
+    
+    @objc func kakaoLoginButtonDidTap() {
+        navigationController?.pushViewController(KakaoConnectViewController(), animated: true)
+    }
+    
 }
--- YELLO-iOS/YELLO-iOS/Presentation/Onboarding/Views/View/KakaoLoginView.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Onboarding/Views/View/KakaoLoginView.swift
@@ -7,14 +7,75 @@
 
 import UIKit
 
-class KakaoLoginView: UIView {
+class KakaoLoginView: BaseView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let titleLabel = UILabel()
+    let subTitleLabel = UILabel()
+    let imageView = UIImageView()
+    let kakaoButton = UIButton()
+    
+    let stackView = UIStackView()
+    
+    override func setStyle() {
+        
+        titleLabel.do {
+            $0.text = "Yell:o에 오신 걸 환영해요!"
+            $0.textColor = .white
+            $0.font = .uiHeadline03
+            $0.setTextWithLineHeight(text: $0.text, lineHeight: 28)
+        }
+        
+        subTitleLabel.do {
+            $0.text = "가입을 시작해볼까요?"
+            $0.font = .uiBodySmall
+            $0.textColor = .grayscales500
+            $0.setTextWithLineHeight(text: $0.text, lineHeight: 22)
+        }
+        
+        imageView.do {
+            $0.image = ImageLiterals.OnBoarding.onboardingStart
+        }
+        
+        stackView.do {
+            $0.addArrangedSubviews(titleLabel, subTitleLabel, imageView)
+            $0.axis = .vertical
+            $0.setCustomSpacing(8, after: subTitleLabel)
+            $0.setCustomSpacing(6, after: subTitleLabel)
+            $0.alignment = .center
+        }
+        
+        kakaoButton.do {
+            $0.setImage(ImageLiterals.OnBoarding.btnKakaoLogin, for: .normal)
+        }
     }
-    */
-
+    
+    override func setLayout() {
+//        self.addSubviews(stackView, kakaoButton)
+//
+//        stackView.snp.makeConstraints {
+//            $0.center.equalToSuperview()
+//        }
+        
+        self.addSubviews(titleLabel,subTitleLabel,imageView,kakaoButton)
+        titleLabel.snp.makeConstraints {
+            $0.bottom.equalTo(subTitleLabel.snp.top).offset(-8)
+            $0.centerX.equalToSuperview()
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.bottom.equalTo(imageView.snp.top).offset(-2)
+            $0.centerX.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints {
+//            $0.top.equalTo(subTitleLabel.snp.top).offset(2)
+//            $0.centerX.equalToSuperview()
+            $0.center.equalToSuperview()
+        }
+        
+        kakaoButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(91)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+    }
 }
