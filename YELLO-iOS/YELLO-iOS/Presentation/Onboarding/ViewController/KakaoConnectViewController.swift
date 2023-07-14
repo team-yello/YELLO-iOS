b26aa268d705c0a574e3422bfaf9eebe966a871e
Zoe <Zoe.urssu@gmail.com>
2023-07-15 01:54:25 +0900

    Add [#49] 이미지 에셋 추가

---
+++ YELLO-iOS/YELLO-iOS/Presentation/Onboarding/ViewController/KakaoConnectViewController.swift
@@ -0,0 +1,29 @@
+//
+//  KakaoConnectViewController.swift
+//  YELLO-iOS
+//
+//  Created by 지희의 MAC on 2023/07/15.
+//
+
+import UIKit
+
+class KakaoConnectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ YELLO-iOS/YELLO-iOS/Presentation/Onboarding/Views/View/KakaoLoginView.swift
@@ -0,0 +1,20 @@
+//
+//  KakaoLoginView.swift
+//  YELLO-iOS
+//
+//  Created by 지희의 MAC on 2023/07/15.
+//
+
+import UIKit
+
+class KakaoLoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
