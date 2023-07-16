e73903c826d114349c9e8498100eb02596878120
정채은 <cce523@hanmail.net>
2023-07-17 05:31:39 +0900

    Feat [#57] Service 추가

--- YELLO-iOS/YELLO-iOS/Network/Recommending/Service/RecommendingService.swift
+++ YELLO-iOS/YELLO-iOS/Network/Recommending/Service/RecommendingService.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+protocol RecommendingServiceProtocol {
+    func recommendingKakaoFriend(queryDTO: RecommendingRequestQueryDTO, requestDTO: RecommendingFriendRequestDTO, completion: @escaping (NetworkResult<BaseResponse<RecommendingFriendResponseDTO>>) -> Void)
+    
+    func recommendingSchoolFriend(queryDTO: RecommendingRequestQueryDTO, completion: @escaping (NetworkResult<BaseResponse<RecommendingFriendResponseDTO>>) -> Void)
+}
+
+final class RecommendingService: APIRequestLoader<RecommendingTarget>, RecommendingServiceProtocol {
+    func recommendingKakaoFriend(queryDTO: RecommendingRequestQueryDTO, requestDTO: RecommendingFriendRequestDTO, completion: @escaping (NetworkResult<BaseResponse<RecommendingFriendResponseDTO>>) -> Void) {
+
+        fetchData(target: .recommendingKakaoFriend(queryDTO, requestDTO),
+                  responseData: BaseResponse<RecommendingFriendResponseDTO>.self, completion: completion)
+    }
+    
+    func recommendingSchoolFriend(queryDTO: RecommendingRequestQueryDTO, completion: @escaping (NetworkResult<BaseResponse<RecommendingFriendResponseDTO>>) -> Void) {
+
+        fetchData(target: .recommendingSchoolFriend(queryDTO),
+                  responseData: BaseResponse<RecommendingFriendResponseDTO>.self, completion: completion)
+    }
+}
