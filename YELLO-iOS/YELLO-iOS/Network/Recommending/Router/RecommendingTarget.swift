7e131c223519c8163344d36550ffab1ee8563886
정채은 <cce523@hanmail.net>
2023-07-17 05:31:24 +0900

    Feat [#57] target 추가

--- YELLO-iOS/YELLO-iOS/Network/Recommending/Router/RecommendingTarget.swift
+++ YELLO-iOS/YELLO-iOS/Network/Recommending/Router/RecommendingTarget.swift
@@ -2,7 +2,43 @@
 //  RecommendingTarget.swift
 //  YELLO-iOS
 //
-//  Created by 변희주 on 2023/07/15.
+//  Created by 정채은 on 2023/07/15.
 //
 
 import Foundation
+
+import Alamofire
+
+enum RecommendingTarget {
+    case recommendingKakaoFriend(_ queryDTO: RecommendingRequestQueryDTO, _ requestDTO: RecommendingFriendRequestDTO)
+    case recommendingSchoolFriend(_ queryDTO: RecommendingRequestQueryDTO)
+}
+
+extension RecommendingTarget: TargetType {
+    var method: HTTPMethod {
+        switch self {
+        case .recommendingKakaoFriend:
+            return .post
+        case .recommendingSchoolFriend:
+            return .get
+        }
+    }
+
+    var path: String {
+        switch self {
+        case .recommendingKakaoFriend(_, _):
+            return "/friend/recommend/kakao"
+        case .recommendingSchoolFriend(_):
+            return "/friend/recommend/school?"
+        }
+    }
+
+    var parameters: RequestParams {
+        switch self {
+        case let .recommendingKakaoFriend(queryDTO, requestDTO):
+            return .requestQueryWithBody(queryDTO, bodyParameter: requestDTO)
+        case let .recommendingSchoolFriend(queryDTO):
+            return .requestQuery(queryDTO)
+        }
+    }
+}
