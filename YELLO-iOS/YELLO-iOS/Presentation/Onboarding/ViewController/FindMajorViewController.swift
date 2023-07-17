c8a0e1b9ac07d87a2327a7503fc95decdcf20b89
Zoe <Zoe.urssu@gmail.com>
2023-07-18 04:12:21 +0900

    Network [#72] 학과 검색 API 연결 및 무한 스크롤 구현

--- YELLO-iOS/YELLO-iOS/Presentation/Onboarding/ViewController/FindMajorViewController.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Onboarding/ViewController/FindMajorViewController.swift
@@ -10,12 +10,62 @@ import UIKit
 class FindMajorViewController: SearchBaseViewController {
     // MARK: - Variables
     // MARK: Property
-    var allMajor: [String] = ["컴퓨터공학과", "컴퓨터학부", "소프트웨어학과", "글로벌미디어학부", "응용소프트웨어학부"]
+    var allMajor: [String] = []
+    var schoolName: String = ""
+    var pageCount: Int = 0
+    var totalItemCount: Int = 0
     
     // MARK: - Function
     // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        super.customView(titleText: "학과 검색하기", helperText: "찾는 과가 없다면 클릭하세요!", allArr: allMajor)
+        super.customView(titleText: "학과 검색하기", helperText: "찾는 과가 없다면 클릭하세요!")
+        addTarget()
+    }
+    
+    func addTarget() {
+        super.searchView.searchTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let tableView = self.searchView.searchResultTableView
+        let offsetY = tableView.contentOffset.y
+        let contentHeight = tableView.contentSize.height
+        let visibleHeight = tableView.bounds.height
+        
+        if offsetY > contentHeight - visibleHeight, allArr.count < totalItemCount {
+            pageCount += 1
+            guard let text = searchView.searchTextField.text else { return }
+            searchMajor(text)
+        }
+    }
+    
+    // MARK: Objc Function
+    func searchMajor(_ word: String) {
+        let queryDTO: MajorSearchRequestQueryDTO = MajorSearchRequestQueryDTO(school: schoolName, search: word, page: pageCount)
+        
+        NetworkService.shared.onboardingService.getMajorList(queryDTO: queryDTO) { [weak self] result in
+            switch result {
+            case .success(let data):
+                guard let data = data.data else { return }
+                self?.allArr.append(contentsOf: data.groupList.map { $0.departmentName })
+                self?.totalItemCount = data.totalCount ?? 0
+                self?.searchView.searchResultTableView.reloadData()
+            default:
+                print(ErrorPointer.self)
+                return
+            }
+        }
+    }
+    
+    // MARK: Objc Function
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        pageCount = 0
+        allMajor.removeAll()
+        allArr.removeAll()
+        searchMajor(text)
     }
 }
+
+
