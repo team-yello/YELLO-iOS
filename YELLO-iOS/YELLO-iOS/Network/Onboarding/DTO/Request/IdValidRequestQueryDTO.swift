c681dc522cc97a1da8aac007e959f4f1ec760fe4
Zoe <Zoe.urssu@gmail.com>
2023-07-18 04:09:27 +0900

    Add [#72] 파일 추가

---
+++ YELLO-iOS/YELLO-iOS/Network/Onboarding/DTO/Request/IdValidRequestQueryDTO.swift
@@ -0,0 +1,12 @@
+//
+//  IdValidRequestQueryDTO.swift
+//  YELLO-iOS
+//
+//  Created by 지희의 MAC on 2023/07/17.
+//
+
+import Foundation
+
+struct IdValidRequestQueryDTO: Codable {
+    let yelloId: String
+}
