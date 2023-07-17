e1354d616f8ae65c1ae796091fbd3e9a5e901c1a
정채은 <cce523@hanmail.net>
2023-07-17 16:51:47 +0900

    Feat [#70] 프로필 Service 설정

--- YELLO-iOS/YELLO-iOS/Network/Profile/Router/ProfileTarget.swift
+++ YELLO-iOS/YELLO-iOS/Network/Profile/Router/ProfileTarget.swift
@@ -2,7 +2,36 @@
 //  ProfileTarget.swift
 //  YELLO-iOS
 //
-//  Created by 변희주 on 2023/07/15.
+//  Created by 정채은 on 2023/07/15.
 //
 
 import Foundation
+
+import Alamofire
+
+enum ProfileTarget {
+    case profileUser(userId: Int)
+}
+
+extension ProfileTarget: TargetType {
+    var method: HTTPMethod {
+        switch self {
+        case .profileUser:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .profileUser(let userId):
+            return "/user/\(userId)"
+        }
+    }
+
+    var parameters: RequestParams {
+        switch self {
+        case .profileUser:
+            return .requestPlain
+        }
+    }
+}
