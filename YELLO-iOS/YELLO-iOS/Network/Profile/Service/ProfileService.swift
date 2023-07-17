5793855cd074a433f1bfa41fc890f3ed293bdf09
정채은 <cce523@hanmail.net>
2023-07-17 16:53:09 +0900

    Feat [#70] 프로필 Service 설정

--- YELLO-iOS/YELLO-iOS/Network/Profile/Service/ProfileService.swift
+++ YELLO-iOS/YELLO-iOS/Network/Profile/Service/ProfileService.swift
@@ -2,7 +2,18 @@
 //  ProfileService.swift
 //  YELLO-iOS
 //
-//  Created by 변희주 on 2023/07/15.
+//  Created by 정채은 on 2023/07/15.
 //
 
 import Foundation
+
+protocol ProfileServiceProtocol {
+    func profileUser(userId: Int, completion: @escaping (NetworkResult<BaseResponse<ProfileUserResponseDTO>>) -> Void)
+}
+
+final class ProfileService: APIRequestLoader<ProfileTarget>, ProfileServiceProtocol {
+    func profileUser(userId: Int, completion: @escaping (NetworkResult<BaseResponse<ProfileUserResponseDTO>>) -> Void) {
+        fetchData(target: .profileUser(userId: userId),
+                  responseData: BaseResponse<ProfileUserResponseDTO>.self, completion: completion)
+    }
+}
