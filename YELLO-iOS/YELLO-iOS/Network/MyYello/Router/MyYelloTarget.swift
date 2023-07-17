0eeee7ee85917323f270efa17b13b2a9bf080595
정채은 <cce523@hanmail.net>
2023-07-18 05:25:11 +0900

    Feat [#74] 내 옐로 Target 추가

--- YELLO-iOS/YELLO-iOS/Network/MyYello/Router/MyYelloTarget.swift
+++ YELLO-iOS/YELLO-iOS/Network/MyYello/Router/MyYelloTarget.swift
@@ -2,7 +2,36 @@
 //  MyYelloTarget.swift
 //  YELLO-iOS
 //
-//  Created by 변희주 on 2023/07/15.
+//  Created by 정채은 on 2023/07/15.
 //
 
 import Foundation
+
+import Alamofire
+
+enum MyYelloTarget {
+    case myYello(_ queryDTO: MyYelloRequestQueryDTO)
+}
+
+extension MyYelloTarget: TargetType {
+    var method: HTTPMethod {
+        switch self {
+        case .myYello:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .myYello(_):
+            return "/vote"
+        }
+    }
+
+    var parameters: RequestParams {
+        switch self {
+        case let .myYello(queryDTO):
+            return .requestQuery(queryDTO)
+        }
+    }
+}
