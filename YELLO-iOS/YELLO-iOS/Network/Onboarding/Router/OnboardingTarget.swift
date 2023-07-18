435c6edbca8de60fe3621ad74b154863a642e634
Zoe <Zoe.urssu@gmail.com>
2023-07-19 07:37:30 +0900

    Network [#82] SignUp API 연결

--- YELLO-iOS/YELLO-iOS/Network/Onboarding/Router/OnboardingTarget.swift
+++ YELLO-iOS/YELLO-iOS/Network/Onboarding/Router/OnboardingTarget.swift
@@ -15,7 +15,7 @@ enum OnboardingTarget {
     case getMajorList(_ dto: MajorSearchRequestQueryDTO) /// 학과 검색
     case getCheckDuplicate(_ dto: IdValidRequestQueryDTO) /// 아이디 중복 확인
     case postFirendsList( _ query: JoinedFriendsRequestQueryDTO, _ dto: JoinedFriendsRequestDTO) /// 가입한 친구 목록 불러오기
-    case postUserInfo(_ accessToken: String, _ requestDTO: SignInRequestDTO ) /// 회원가입
+    case postUserInfo(_ dto: SignUpRequestDTO ) /// 회원가입
 }
 
 extension OnboardingTarget: TargetType {
@@ -29,7 +29,7 @@ extension OnboardingTarget: TargetType {
             return .get
         case .getCheckDuplicate:
             return .get
-        case .postFirendsList(_):
+        case .postFirendsList:
             return .post
         case .postUserInfo:
             return .post
@@ -44,7 +44,7 @@ extension OnboardingTarget: TargetType {
             return "/auth/school/school"
         case .getCheckDuplicate(let id):
             return "/auth/valid"
-        case .postUserInfo(_, _):
+        case .postUserInfo(_):
             return "/auth/signup"
         case .getMajorList(_):
             return "/auth/school/department"
@@ -61,11 +61,11 @@ extension OnboardingTarget: TargetType {
             return .requestQuery(dto)
         case .getCheckDuplicate(let dto):
             return .requestQuery(dto)
-        case .postUserInfo(let acessToken, let dto):
-            return .requestQueryWithBody(acessToken, bodyParameter: dto)
+        case .postUserInfo(let dto):
+            return .requestWithBody(dto)
         case .getMajorList(let dto):
             return .requestQuery(dto)
-        case .postFirendsList(let query, let dto):
+        case .postFirendsList(let query,let dto):
             return .requestQueryWithBody(query, bodyParameter: dto)
         }
     }
