d814188c0fbf4d6c2c24209258666bea24a9addd
Zoe <Zoe.urssu@gmail.com>
2023-07-19 00:08:03 +0900

    NetWork [#78] 추천인 코드 검색 API 연결

--- YELLO-iOS/YELLO-iOS/Presentation/Onboarding/ViewController/RecommendIdViewController.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Onboarding/ViewController/RecommendIdViewController.swift
@@ -9,29 +9,32 @@ import UIKit
 
 class RecommendIdViewController: OnboardingBaseViewController {
     // MARK: - Variables
-    // MARK: Component 
+    var isExisted = false
+    
+    // MARK: Component
     let baseView = RecommendIdView()
+    let text = ""
     
     // MARK: - Function
     // MARK: LifeCycle
     override func loadView() {
-            super.loadView()
-            super.isSkipable = true
-            super.nextViewController = OnboardingEndViewController()
-            view = baseView
-        }
-        
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            navigationController?.navigationBar.isHidden = true
-        }
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            setDelegate()
-            addTarget()
-        }
-        
+        super.loadView()
+        super.isSkipable = true
+        super.nextViewController = OnboardingEndViewController()
+        view = baseView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDelegate()
+        addTarget()
+    }
+    
     // MARK: Custom Function
     func setDelegate() {
         baseView.recommendIdTextField.textField.delegate = self
@@ -46,14 +49,50 @@ class RecommendIdViewController: OnboardingBaseViewController {
         let idText = baseView.recommendIdTextField.textField.text ?? ""
         let isValidId = idText.isId()
         
-        if !isValidId {
+        if !isExisted {
             idTextFieldView.textField.setButtonState(state: .error)
             idTextFieldView.helperLabel.setLabelStyle(text: "존재하지 않는 아이디에요", State: .error)
+            nextButton.setButtonEnable(state: false)
+            return
         }
         
-        let isButtonEnabled = !(idText.isEmpty) && isValidId
+        let isButtonEnabled = !(idText.isEmpty) && isValidId && isExisted
+        if isButtonEnabled {
+            baseView.recommendIdTextField.textField.setButtonState(state: .done)
+            baseView.recommendIdTextField.helperLabel.setLabelStyle(text: "", State: .done)
+            baseView.recommendIdTextField.textField.rightViewMode = .never
+        }
         nextButton.setButtonEnable(state: isButtonEnabled)
     }
+    
+    func checkIdValid(text: String) {
+        let queryDTO = IdValidRequestQueryDTO(yelloId: text)
+        
+        NetworkService.shared.onboardingService.getCheckDuplicate(queryDTO: queryDTO) { [weak self] result in
+            switch result {
+            case .success(let data):
+                DispatchQueue.main.async {
+                    guard let data = data.data else {
+                        if data.status == 404 {
+                            self?.isExisted = false
+                            self?.checkButtonEnable()
+                        }
+                        return
+                    }
+                    if data {
+                        self?.isExisted = true
+                    }
+                    
+                    self?.checkButtonEnable()
+                    
+                }
+            default:
+                DispatchQueue.main.async {
+                    self?.checkButtonEnable()
+                }
+            }
+        }
+            }
     
     // MARK: Objc Function
     @objc func idCancelTapped() {
@@ -75,6 +114,8 @@ extension RecommendIdViewController: UITextFieldDelegate {
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         baseView.recommendIdTextField.textField.setButtonState(state: .id)
+        guard let text = baseView.recommendIdTextField.textField.text else { return }
+        checkIdValid(text: text)
         checkButtonEnable()
     }
     
