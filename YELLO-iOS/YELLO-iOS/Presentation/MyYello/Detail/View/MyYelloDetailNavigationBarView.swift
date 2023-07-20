1b682f1f1598adb3601d50a45e78078b15825104
정채은 <cce523@hanmail.net>
2023-07-20 20:13:08 +0900

    Fix [#109] 포인트 부족 시 포인트 레이아웃 재설정

--- YELLO-iOS/YELLO-iOS/Presentation/MyYello/Detail/View/MyYelloDetailNavigationBarView.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/MyYello/Detail/View/MyYelloDetailNavigationBarView.swift
@@ -68,7 +68,6 @@ final class MyYelloDetailNavigationBarView: BaseView {
         
         pointImageView.snp.makeConstraints {
             $0.centerY.equalTo(backButton)
-//            $0.trailing.equalToSuperview().inset(61)
         }
         
         pointLabel.snp.makeConstraints {
--- YELLO-iOS/YELLO-iOS/Presentation/MyYello/Detail/View/PointLackView.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/MyYello/Detail/View/PointLackView.swift
@@ -68,13 +68,14 @@ final class PointLackView: BaseView {
         }
         
         pointLabel.do {
-            $0.setTextWithLineHeight(text: "2350", lineHeight: 20)
+            $0.text = "0"
             $0.textColor = .white
             $0.font = .uiKeywordBold
+            $0.textAlignment = .right
         }
         
         pointTextLabel.do {
-            $0.setTextWithLineHeight(text: StringLiterals.MyYello.Alert.point, lineHeight: 20)
+            $0.text = StringLiterals.MyYello.Alert.point
             $0.textColor = .grayscales400
             $0.font = .uiBodySmall
         }
@@ -135,12 +136,13 @@ final class PointLackView: BaseView {
         
         pointLabel.snp.makeConstraints {
             $0.trailing.equalTo(pointTextLabel.snp.leading).inset(-4)
-            $0.centerY.equalToSuperview()
+            $0.centerY.equalTo(pointTitleLabel)
+            $0.width.equalTo(103)
         }
         
         pointTextLabel.snp.makeConstraints {
             $0.trailing.equalToSuperview().inset(16)
-            $0.centerY.equalToSuperview()
+            $0.centerY.equalTo(pointTitleLabel)
         }
         
         yelloButton.snp.makeConstraints {
