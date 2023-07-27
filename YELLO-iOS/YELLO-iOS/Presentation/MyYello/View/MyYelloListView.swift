e19350cdcd9cc687a134d1206b4a7642d325011d
정채은 <cce523@hanmail.net>
2023-07-27 15:37:26 +0900

    Fix [#124] 내 옐로 테이블 셀 하나 선택 시 다른 셀 터치 이벤트 막기
    0.5초 간 터치 불가능하게 막음

--- YELLO-iOS/YELLO-iOS/Presentation/MyYello/View/MyYelloListView.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/MyYello/View/MyYelloListView.swift
@@ -272,5 +272,11 @@ extension MyYelloListView: UITableViewDataSource {
             return
         }
         self.pushMyYelloDetailViewController(index: indexPath.row)
+        tableView.isUserInteractionEnabled = false
+        
+        // 일정 시간(예: 0.5초) 이후에 다시 사용자 상호 작용을 활성화
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            tableView.isUserInteractionEnabled = true
+        }
     }
 }
