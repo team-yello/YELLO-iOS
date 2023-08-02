8b3bbe51da2f6f26624eb1888bad0c13910e1a6e
정채은 <cce523@hanmail.net>
2023-08-02 18:51:50 +0900

    Feat [#143] 추천친구 navigationbar 설정

---
+++ YELLO-iOS/YELLO-iOS/Presentation/Recommending/FriendSearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FriendSearchViewController.swift
+//  YELLO-iOS
+//
+//  Created by 정채은 on 2023/08/02.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class FriendSearchViewController: BaseViewController {
+
+
+}
