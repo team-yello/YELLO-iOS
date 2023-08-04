5b1a98cd5a1e8bcee4edd33a6640c9ee7a45e552
Zoe <Zoe.urssu@gmail.com>
2023-08-04 21:27:25 +0900

    Fix [#139] 변경된 radius 추가

--- YELLO-iOS/YELLO-iOS/Presentation/Onboarding/Constraints.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Onboarding/Constraints.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class Constraints {
     static var round = 8
+    static var buttonRound = 24
     static var bigMargin = 16.adjusted
     static var smallMargin = 12.adjusted
     static var topMargin = 20.adjusted
