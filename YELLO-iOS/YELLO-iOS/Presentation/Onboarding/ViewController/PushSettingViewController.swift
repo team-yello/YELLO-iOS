673239d78f2bacba7a0bf760990cbd1d5fe312ba
Zoe <Zoe.urssu@gmail.com>
2023-08-08 17:38:58 +0900

    Fix [#139] 알림 설정 코드 이동

--- YELLO-iOS/YELLO-iOS/Presentation/Onboarding/ViewController/PushSettingViewController.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Onboarding/ViewController/PushSettingViewController.swift
@@ -12,6 +12,7 @@ class PushSettingViewController: UIViewController {
     // MARK: Component (button, label 등 코드로 만들때)
     let baseView = PushSettingView()
     let onboardingEndViewController = OnboardingEndViewController()
+    let userNotiCenter = UNUserNotificationCenter.current()
     
     // MARK: Life Cycles
     override func viewWillAppear(_ animated: Bool) {
@@ -20,6 +21,7 @@ class PushSettingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(User.shared)
         setUI()
     }
     
@@ -30,6 +32,9 @@ class PushSettingViewController: UIViewController {
     
     private func setStyle() {
         view.backgroundColor = .black
+        baseView.do {
+            $0.goToYelloButton.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
+        }
     }
     
     private func setLayout() {
@@ -39,7 +44,18 @@ class PushSettingViewController: UIViewController {
         }
     }
     
+    // 사용자에게 알림 권한 요청
+    func requestAuthNoti() {
+        let notiAuthOptions = UNAuthorizationOptions(arrayLiteral: [.alert, .badge, .sound])
+        userNotiCenter.requestAuthorization(options: notiAuthOptions) { (success, error) in
+            if let error = error {
+                print(#function, error)
+            }
+        }
+    }
+    
     @objc func tapButton() {
+        requestAuthNoti()
         navigationController?.pushViewController(onboardingEndViewController, animated: true)
     }
 
