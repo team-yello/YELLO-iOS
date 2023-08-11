529c2795475acba88ab041d849351c02102d754f
Zoe <Zoe.urssu@gmail.com>
2023-08-11 18:52:29 +0900

    Chore [#160] 주석 제거

--- YELLO-iOS/YELLO-iOS/Global/Extensions/UIWindow+.swift
+++ YELLO-iOS/YELLO-iOS/Global/Extensions/UIWindow+.swift
@@ -13,15 +13,6 @@ extension UIWindow {
         return self.visibleViewControllerFrom(vc: self.rootViewController)
     }
     
-    /**
-     # visibleViewControllerFrom
-     - Author: suni
-     - Date:
-     - Parameters:
-        - vc: rootViewController 혹은 UITapViewController
-     - Returns: UIViewController?
-     - Note: vc내에서 가장 최상위에 있는 뷰컨트롤러 반환
-    */
     public func visibleViewControllerFrom(vc: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
         if let nc = vc as? UINavigationController {
             return self.visibleViewControllerFrom(vc: nc.visibleViewController)
