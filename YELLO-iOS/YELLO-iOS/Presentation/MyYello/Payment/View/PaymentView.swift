efe9baa6bdc3d4e4e75716a6149683e65a661406
chaentopia <cce523@hanmail.net>
2023-08-11 14:01:54 +0900

    Feat [#159] 결제 pageControl 수정

--- YELLO-iOS/YELLO-iOS/Presentation/MyYello/Payment/View/PaymentView.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/MyYello/Payment/View/PaymentView.swift
@@ -24,7 +24,7 @@ final class PaymentView: BaseView {
     }
     
     lazy var collectionView = UICollectionView(frame: .zero,
-                                                       collectionViewLayout: self.flowLayout).then {
+                                               collectionViewLayout: self.flowLayout).then {
         $0.register(PaymentCollectionViewCell.self, forCellWithReuseIdentifier: PaymentCollectionViewCell.paymentIdentifier)
         $0.showsHorizontalScrollIndicator = false
         $0.showsVerticalScrollIndicator = false
@@ -33,7 +33,7 @@ final class PaymentView: BaseView {
         $0.isPagingEnabled = true
         $0.backgroundColor = .clear
     }
-    
+    lazy var pageControl = UIPageControl()
     let pageControlStackView = UIStackView()
     let pageControlFirst = UIView(frame: CGRect(x: 0, y: 0, width: 6, height: 6))
     let pageControlSecond = UIView(frame: CGRect(x: 0, y: 0, width: 6, height: 6))
@@ -48,25 +48,17 @@ final class PaymentView: BaseView {
             $0.addArrangedSubviews(pageControlFirst, pageControlSecond, pageControlThird)
         }
         
-        pageControlFirst.do {
-            $0.backgroundColor = .white
-            $0.makeCornerRound(radius: 3)
-        }
-        
-        pageControlSecond.do {
-            $0.backgroundColor = .grayscales700
-            $0.makeCornerRound(radius: 3)
-        }
-        
-        pageControlThird.do {
-            $0.backgroundColor = .grayscales700
-            $0.makeCornerRound(radius: 3)
+        pageControl.do {
+            $0.numberOfPages = 3
+            $0.currentPage = 0
+            $0.pageIndicatorTintColor = .grayscales700
+            $0.currentPageIndicatorTintColor = .white
         }
     }
     
     override func setLayout() {
         self.addSubviews(collectionView,
-                         pageControlStackView)
+                         pageControl)
         
         collectionView.snp.makeConstraints {
             $0.top.equalToSuperview()
@@ -75,23 +67,9 @@ final class PaymentView: BaseView {
             $0.width.equalTo(375.adjustedWidth)
         }
         
-        pageControlFirst.snp.makeConstraints {
-            $0.width.height.equalTo(6)
-        }
-        
-        pageControlSecond.snp.makeConstraints {
-            $0.width.height.equalTo(6)
-        }
-        
-        pageControlThird.snp.makeConstraints {
-            $0.width.height.equalTo(6)
-        }
-        
-        pageControlStackView.snp.makeConstraints {
+        pageControl.snp.makeConstraints {
             $0.centerX.equalTo(collectionView)
-            $0.top.equalTo(collectionView.snp.bottom).offset(3.adjustedHeight)
-            $0.height.equalTo(6)
-            $0.width.equalTo(34)
+            $0.top.equalTo(collectionView.snp.bottom).offset(-3.adjustedHeight)
         }
     }
 }
@@ -108,15 +86,18 @@ extension PaymentView: UICollectionViewDelegate, UICollectionViewDataSource {
     }
     
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        let pageWidth = collectionView.bounds.size.width
-        let currentPage = Int(targetContentOffset.pointee.x / pageWidth)
-        updatePageControlForPage(currentPage)
+        let page = Int(targetContentOffset.pointee.x / self.frame.width)
+        self.pageControl.currentPage = page
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let pageWidth = scrollView.bounds.size.width
-        let currentPage = Int((scrollView.contentOffset.x + (pageWidth / 2)) / pageWidth)
-        updatePageControlForPage(currentPage)
+        let width = scrollView.bounds.size.width
+        let x = scrollView.contentOffset.x + (width/2)
+        
+        let newPage = Int(x/width)
+        if pageControl.currentPage != newPage {
+            pageControl.currentPage = newPage
+        }
     }
     
     private func updatePageControlForPage(_ page: Int) {
@@ -127,7 +108,8 @@ extension PaymentView: UICollectionViewDelegate, UICollectionViewDataSource {
             self.pageControlThird.backgroundColor = (page == 2) ? .white : .grayscales700
         }
     }
-    //컬렉션뷰 감속 끝났을 때 현재 페이지 체크
+    
+    /// 컬렉션뷰 감속 끝났을 때 현재 페이지 체크
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         nowPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
     }
@@ -140,23 +122,23 @@ extension PaymentView: UICollectionViewDelegateFlowLayout {
 }
 
 extension PaymentView {
-    // 3초마다 실행되는 타이머
-        func bannerTimer() {
-            let _: Timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (Timer) in
-                self.bannerMove()
-            }
+    /// 3초마다 실행되는 타이머
+    func bannerTimer() {
+        let _: Timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (Timer) in
+            self.bannerMove()
         }
-        // 배너 움직이는 매서드
-        func bannerMove() {
-            // 현재페이지가 마지막 페이지일 경우
-            if nowPage == paymentImage.count-1 {
-            // 맨 처음 페이지로 돌아감
-                collectionView.scrollToItem(at: NSIndexPath(item: 0, section: 0) as IndexPath, at: .right, animated: true)
-                nowPage = 0
-                return
-            }
-            // 다음 페이지로 전환
-            nowPage += 1
-            collectionView.scrollToItem(at: NSIndexPath(item: nowPage, section: 0) as IndexPath, at: .right, animated: true)
+    }
+    // 배너 움직이는 매서드
+    func bannerMove() {
+        /// 현재페이지가 마지막 페이지일 경우
+        if nowPage == paymentImage.count-1 {
+            /// 맨 처음 페이지로 돌아감
+            collectionView.scrollToItem(at: NSIndexPath(item: 0, section: 0) as IndexPath, at: .right, animated: true)
+            nowPage = 0
+            return
         }
+        /// 다음 페이지로 전환
+        nowPage += 1
+        collectionView.scrollToItem(at: NSIndexPath(item: nowPage, section: 0) as IndexPath, at: .right, animated: true)
+    }
 }
