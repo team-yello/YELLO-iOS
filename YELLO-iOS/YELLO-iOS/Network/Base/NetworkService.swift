7214fbe01d485cc50d5ee2496ec1bb61cf80aa89
정채은 <cce523@hanmail.net>
2023-08-14 21:46:22 +0900

    Chore [#174] 불필요한 주석 삭제

--- YELLO-iOS/YELLO-iOS/Network/Base/NetworkService.swift
+++ YELLO-iOS/YELLO-iOS/Network/Base/NetworkService.swift
@@ -12,12 +12,10 @@ final class NetworkService {
 
     private init() {}
 
-//    let onboardingService: OnboardingServiceProtocol = OnboardingService(apiLogger: APIEventLogger())
     let votingService: VotingServiceProtocol = VotingService(apiLogger: APIEventLogger())
     let onboardingService: OnboardingServiceProtocol = OnboardingService(apiLogger: APIEventLogger())
     let aroundService: AroundServiceProtocol = AroundService(apiLogger: APIEventLogger())
     let recommendingService: RecommendingServiceProtocol = RecommendingService(apiLogger: APIEventLogger())
-//    let votingService: VotingServiceProtocol = VotingService(apiLogger: APIEventLogger())
     let myYelloService: MyYelloServiceProtocol = MyYelloService(apiLogger: APIEventLogger())
     let profileService: ProfileServiceProtocol = ProfileService(apiLogger: APIEventLogger())
 }
