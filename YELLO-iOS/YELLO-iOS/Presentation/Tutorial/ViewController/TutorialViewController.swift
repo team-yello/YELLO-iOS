fb1b8ff534502e334b6b30761c1073e64ac1a132
Zoe <Zoe.urssu@gmail.com>
2023-08-16 00:39:28 +0900

    Feat [#180] 화면 전환

--- YELLO-iOS/YELLO-iOS/Presentation/Tutorial/ViewController/TutorialViewController.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Tutorial/ViewController/TutorialViewController.swift
@@ -9,26 +9,53 @@ import UIKit
 
 class TutorialViewController: UIViewController {
     
-    var pageCount = 0
-    
-    let baseView = FirstTutorialView()
-    
-    override func loadView() {
-        view = baseView
-    }
+    var pageCount = 1
+
+    var baseView: UIView = FirstTutorialView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
         addTarget()
     }
     
+    private func setUI(){
+        setStyle()
+        setLayout()
+    }
+    
+    private func setStyle() {    }
+    
+    private func setLayout() {
+        view.addSubview(baseView)
+        
+        baseView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
     private func addTarget() {
-        baseView.tutorialImageView.isUserInteractionEnabled = true
-        baseView.tutorialImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.tapView)))
+        baseView.isUserInteractionEnabled = true
+        baseView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.tapView)))
     }
     
     @objc func tapView() {
+        pageCount += 1
         
+        if pageCount < 5 {
+            switch pageCount {
+            case 2:
+                baseView = SecondTutorialView()
+            case 3:
+                baseView = ThirdTutorialView()
+            case 4:
+                baseView = FourthTutorial()
+            default:
+                baseView = UIView()
+            }
+            setUI()
+            addTarget()
+        }
     }
     
 }
