fa477d52ef5cd9d54bd067785d54e24a44bc6c0c
정채은 <cce523@hanmail.net>
2023-08-17 16:56:42 +0900

    Network [#182] 결제 Service

---
+++ YELLO-iOS/YELLO-iOS/Network/Purchase/Service/PurchaseService.swift
@@ -0,0 +1,30 @@
+//
+//  PurchaseService.swift
+//  YELLO-iOS
+//
+//  Created by 정채은 on 2023/08/17.
+//
+
+import Foundation
+
+protocol PurchaseServiceProtocol {
+
+    func purchaseSubscibe(requestDTO: PurchaseRequestDTO, completion: @escaping (NetworkResult<BaseResponse<String>>) -> Void)
+    
+    func purchaseTicket(requestDTO: PurchaseRequestDTO, completion: @escaping (NetworkResult<BaseResponse<String>>) -> Void)
+    
+}
+
+final class PurchaseService: APIRequestLoader<PurchaseTarget>, PurchaseServiceProtocol {
+    func purchaseSubscibe(requestDTO: PurchaseRequestDTO, completion: @escaping (NetworkResult<BaseResponse<String>>) -> Void) {
+        
+        fetchData(target: .purchaseSubscibe(requestDTO),
+                  responseData: BaseResponse<String>.self, completion: completion)
+    }
+    
+    func purchaseTicket(requestDTO: PurchaseRequestDTO, completion: @escaping (NetworkResult<BaseResponse<String>>) -> Void) {
+        
+        fetchData(target: .purchaseTicket(requestDTO),
+                  responseData: BaseResponse<String>.self, completion: completion)
+    }
+}
