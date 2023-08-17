4a05ee4ca832e41aa38e1c3842a1d34e2c6e2f55
정채은 <cce523@hanmail.net>
2023-08-17 16:30:48 +0900

    Fix [#190] 결제 viewDidLoad 함수로 바꾸기

--- YELLO-iOS/YELLO-iOS/Network/Purchase/Service/IAPService.swift
+++ YELLO-iOS/YELLO-iOS/Network/Purchase/Service/IAPService.swift
@@ -8,159 +8,152 @@
 import StoreKit
 
 extension Notification.Name {
-  static let iapServicePurchaseNotification = Notification.Name("IAPServicePurchaseNotification")
+    static let iapServicePurchaseNotification = Notification.Name("IAPServicePurchaseNotification")
 }
 
 typealias ProductsRequestCompletion = (_ success: Bool, _ products: [SKProduct]?) -> Void
 
 protocol IAPServiceType {
-  var canMakePayments: Bool { get }
-  
-  func getProducts(completion: @escaping ProductsRequestCompletion)
-  func buyProduct(_ product: SKProduct)
-  func isProductPurchased(_ productID: String) -> Bool
-  func restorePurchases()
+    var canMakePayments: Bool { get }
+    
+    func getProducts(completion: @escaping ProductsRequestCompletion)
+    func buyProduct(_ product: SKProduct)
+    func isProductPurchased(_ productID: String) -> Bool
+    func restorePurchases()
 }
 
 final class IAPService: NSObject, IAPServiceType {
-  private let productIDs: Set<String>
-  private var purchasedProductIDs: Set<String>
-  private var productsRequest: SKProductsRequest?
-  private var productsCompletion: ProductsRequestCompletion?
-  
-  var canMakePayments: Bool {
-    SKPaymentQueue.canMakePayments()
-  }
-  
-  init(productIDs: Set<String>) {
-    self.productIDs = productIDs
-//      self.purchasedProductIDs = productIDs
-//          .filter { UserDefaults.standard.bool(forKey: $0) == true }
-      self.purchasedProductIDs = Set<String>()
+    private let productIDs: Set<String>
+    private var purchasedProductIDs: Set<String>
+    private var productsRequest: SKProductsRequest?
+    private var productsCompletion: ProductsRequestCompletion?
     
-    super.init()
-    SKPaymentQueue.default().add(self)
-      print("채은1")
-  }
-  
-  func getProducts(completion: @escaping ProductsRequestCompletion) {
-    self.productsRequest?.cancel()
-    self.productsCompletion = completion
-    self.productsRequest = SKProductsRequest(productIdentifiers: self.productIDs)
-    self.productsRequest?.delegate = self
-    self.productsRequest?.start()
-      print("채은2")
-  }
+    var canMakePayments: Bool {
+        SKPaymentQueue.canMakePayments()
+    }
     
-  func buyProduct(_ product: SKProduct) {
-    SKPaymentQueue.default().add(SKPayment(product: product))
-      print("채은3")
-  }
+    init(productIDs: Set<String>) {
+        self.productIDs = productIDs
+        //      self.purchasedProductIDs = productIDs
+        //          .filter { UserDefaults.standard.bool(forKey: $0) == true }
+        self.purchasedProductIDs = Set<String>()
+        
+        super.init()
+        SKPaymentQueue.default().add(self)
+        print("채은1")
+    }
     
-  func isProductPurchased(_ productID: String) -> Bool {
-    self.purchasedProductIDs.contains(productID)
-  }
+    func getProducts(completion: @escaping ProductsRequestCompletion) {
+        self.productsRequest?.cancel()
+        self.productsCompletion = completion
+        self.productsRequest = SKProductsRequest(productIdentifiers: self.productIDs)
+        self.productsRequest?.delegate = self
+        self.productsRequest?.start()
+        print("채은2")
+    }
     
-  func restorePurchases() {
-    SKPaymentQueue.default().restoreCompletedTransactions()
-      print("채은5")
-
-  }
+    func buyProduct(_ product: SKProduct) {
+        SKPaymentQueue.default().add(SKPayment(product: product))
+        print("채은3")
+    }
+    
+    func isProductPurchased(_ productID: String) -> Bool {
+        self.purchasedProductIDs.contains(productID)
+    }
+    
+    func restorePurchases() {
+        SKPaymentQueue.default().restoreCompletedTransactions()
+        print("채은5")
+        
+    }
 }
 
 extension IAPService: SKProductsRequestDelegate {
-  // didReceive
-  func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
-    let products = response.products
-    self.productsCompletion?(true, products)
-    self.clearRequestAndHandler()
-      print("채은6")
-
-      products.forEach { print("Found product: \($0.productIdentifier) \($0.localizedTitle) \($0.price.floatValue)") }
-  }
-  
-  // failed
-  func request(_ request: SKRequest, didFailWithError error: Error) {
-    print("Erorr: \(error.localizedDescription)")
-    self.productsCompletion?(false, nil)
-    self.clearRequestAndHandler()
-      print("채은7")
-
-  }
-  
-  private func clearRequestAndHandler() {
-    self.productsRequest = nil
-    self.productsCompletion = nil
-      print("채은8")
-
-  }
+    // didReceive
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        let products = response.products
+        self.productsCompletion?(true, products)
+        self.clearRequestAndHandler()
+        print("채은6")
+        
+        products.forEach { print("Found product: \($0.productIdentifier) \($0.localizedTitle) \($0.price.floatValue)") }
+    }
+    
+    // failed
+    func request(_ request: SKRequest, didFailWithError error: Error) {
+        print("Erorr: \(error.localizedDescription)")
+        self.productsCompletion?(false, nil)
+        self.clearRequestAndHandler()
+        print("채은7")
+        
+    }
+    
+    private func clearRequestAndHandler() {
+        self.productsRequest = nil
+        self.productsCompletion = nil
+        print("채은8")
+        
+    }
 }
 
 extension IAPService: SKPaymentTransactionObserver {
-  func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-    transactions.forEach {
-      switch $0.transactionState {
-      case .purchased:
-          let productID = $0.payment.productIdentifier
-          self.deliverPurchaseNotificationFor(id: productID, transaction: $0)
-          print("completed transaction")
-          SKPaymentQueue.default().finishTransaction($0)
-          print("채은888888")
-
-      case .failed:
-          if let transactionError = $0.error as NSError?,
-           let description = $0.error?.localizedDescription,
-           transactionError.code != SKError.paymentCancelled.rawValue {
-          print("Transaction erorr: \(description)")
-              print("채은9")
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        transactions.forEach {
+            switch $0.transactionState {
+            case .purchased:
+                let productID = $0.payment.productIdentifier
+                self.deliverPurchaseNotificationFor(id: productID, transaction: $0)
+                print("completed transaction")
+                SKPaymentQueue.default().finishTransaction($0)
+                print("채은888888")
+                
+            case .failed:
+                if let transactionError = $0.error as NSError?,
+                   let description = $0.error?.localizedDescription,
+                   transactionError.code != SKError.paymentCancelled.rawValue {
+                    print("Transaction erorr: \(description)")
+                    print("채은9")
+                }
+                SKPaymentQueue.default().finishTransaction($0)
+                NotificationCenter.default.post(name: Notification.Name("HideLoadingIndicator"), object: nil)
+                
+            case .restored:
+                print("failed transaction")
+                self.deliverPurchaseNotificationFor(id: $0.original?.payment.productIdentifier, transaction: $0)
+                SKPaymentQueue.default().finishTransaction($0)
+                print("채은10")
+                
+            case .deferred:
+                print("deferred")
+                print("채은11")
+                
+            case .purchasing:
+                print("purchasing")
+                print("채은12")
+                
+            default:
+                print("unknown")
+                print("채은13")
+            }
         }
-        SKPaymentQueue.default().finishTransaction($0)
-          NotificationCenter.default.post(name: Notification.Name("HideLoadingIndicator"), object: nil)
-          
-      case .restored:
-        print("failed transaction")
-          self.deliverPurchaseNotificationFor(id: $0.original?.payment.productIdentifier, transaction: $0)
-        SKPaymentQueue.default().finishTransaction($0)
-          print("채은10")
-
-      case .deferred:
-        print("deferred")
-          print("채은11")
-
-      case .purchasing:
-        print("purchasing")
-          print("채은12")
-
-      default:
-        print("unknown")
-          print("채은13")
-      }
     }
-  }
     
     private func deliverPurchaseNotificationFor(id: String?, transaction: SKPaymentTransaction) {
-      guard let id = id else {
-          print("productID is nil")
-          return
-      }
+        guard let id = id else {
+            print("productID is nil")
+            return
+        }
         
         let transactionID = transaction.transactionIdentifier ?? ""
         print("Transaction ID: \(transactionID)")
-//        
-//        if isProductPurchased(id) {
-//            NotificationCenter.default.post(name: Notification.Name("HideLoadingIndicator"), object: nil)
-//
-//            SKPaymentQueue.default().finishTransaction(transaction)
-//            return
-//        }
-
-      self.purchasedProductIDs.insert(id)
-      UserDefaults.standard.set(true, forKey: id)
-      print("채은14")
-      NotificationCenter.default.post(
-        name: .iapServicePurchaseNotification,
-        object: id
-      )
-      print("Notification delivered for product ID: \(id)")
-  }
+        
+        self.purchasedProductIDs.insert(id)
+        UserDefaults.standard.set(true, forKey: id)
+        print("채은14")
+        NotificationCenter.default.post(
+            name: .iapServicePurchaseNotification,
+            object: id
+        )
+        print("Notification delivered for product ID: \(id)")
+    }
 }
--- YELLO-iOS/YELLO-iOS/Presentation/Payment/ViewController/PaymentPlusViewController.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Payment/ViewController/PaymentPlusViewController.swift
@@ -40,7 +40,20 @@ final class PaymentPlusViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setDelegate()
+        getProducts()
+        setNotification()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
+        tabBarController?.tabBar.isHidden = true
+        paymentPlusView.paymentView.bannerTimer()
+        print("채은16")
+        
+    }
+    
+    private func getProducts() {
         MyProducts.iapService.getProducts { [self] success, products in
             print("load products \(products ?? [])")
             if success, let products = products {
@@ -50,32 +63,25 @@ final class PaymentPlusViewController: BaseViewController {
                     print(self.products)
                     self.setAddTarget()
                     print("채은15")
-
+                    
                 }
             }
         }
-        
+    }
+    
+    private func setNotification() {
         NotificationCenter.default.addObserver(
             self,
             selector: #selector(hideLoadingIndicator),
             name: Notification.Name("HideLoadingIndicator"),
             object: nil)
-
-        NotificationCenter.default.addObserver(
-          self,
-          selector: #selector(handlePurchaseNoti(_:)),
-          name: .iapServicePurchaseNotification,
-          object: nil
-        )
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         
-        tabBarController?.tabBar.isHidden = true
-        paymentPlusView.paymentView.bannerTimer()
-        print("채은16")
-
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handlePurchaseNoti(_:)),
+            name: .iapServicePurchaseNotification,
+            object: nil
+        )
     }
     
     override func setLayout() {
@@ -116,7 +122,7 @@ extension PaymentPlusViewController {
     @objc private func paymentYelloPlusButtonTapped() {
         showLoadingIndicator()
         print("채은17")
-
+        
         MyProducts.iapService.buyProduct(products[0])
         print("옐로플러스 구독 결제")
     }
@@ -124,7 +130,7 @@ extension PaymentPlusViewController {
     @objc private func paymentNameKeyOneButtonTapped() {
         showLoadingIndicator()
         print("채은18")
-
+        
         MyProducts.iapService.buyProduct(products[1])
         print("이름 열람권 1개 구입")
     }
@@ -132,7 +138,7 @@ extension PaymentPlusViewController {
     @objc private func paymentNameKeyTwoButtonTapped() {
         showLoadingIndicator()
         print("채은19")
-
+        
         MyProducts.iapService.buyProduct(products[2])
         print("이름 열람권 2개 구입")
     }
@@ -140,14 +146,14 @@ extension PaymentPlusViewController {
     @objc private func paymentNameKeyFiveButtonTapped() {
         showLoadingIndicator()
         print("채은20")
-
+        
         MyProducts.iapService.buyProduct(products[3])
         print("이름 열람권 5개 구입")
     }
     
     func showPaymentConfirmView(state: PaymentStatus) {
         print("채은21")
-
+        
         guard let viewController = UIApplication.shared.keyWindow?.rootViewController else { return }
         paymentConfirmView.removeFromSuperview()
         paymentConfirmView = PaymentConfirmView()
@@ -184,7 +190,7 @@ extension PaymentPlusViewController {
     
     @objc private func restore() {
         print("채은22")
-
+        
         MyProducts.iapService.restorePurchases()
     }
     
@@ -193,22 +199,22 @@ extension PaymentPlusViewController {
         
         // 상품 구매
         if productID == MyProducts.nameKeyOneProductID ||
-           productID == MyProducts.nameKeyTwoProductID ||
-           productID == MyProducts.nameKeyFiveProductID {
+            productID == MyProducts.nameKeyTwoProductID ||
+            productID == MyProducts.nameKeyFiveProductID {
             verifyConsumablePurchase(productID: productID)
             print("채은23")
-
+            
         }
         
         // 구독 상품 구매
         if productID == MyProducts.yelloPlusProductID {
             verifySubscriptionPurchase()
             print("채은24")
-
+            
         }
         print("여기")
     }
-
+    
     private func verifyConsumablePurchase(productID: String) {
         // 여기에 상품 구매에 대한 처리를 추가합니다.
         let productID = productID
@@ -217,29 +223,29 @@ extension PaymentPlusViewController {
         case MyProducts.nameKeyOneProductID:
             showPaymentConfirmView(state: .nameKeyOne)
             print("채은25")
-
+            
         case MyProducts.nameKeyTwoProductID:
             showPaymentConfirmView(state: .nameKeyTwo)
             print("채은26")
-
+            
         case MyProducts.nameKeyFiveProductID:
             showPaymentConfirmView(state: .nameKeyFive)
             print("채은27")
-
+            
         default:
             return
             print("채은28")
-
+            
         }
         print("상품 구매 완료: \(productID)")
         // 서버와 통신 등의 작업을 수행합니다.
     }
-
+    
     private func verifySubscriptionPurchase() {
         // 여기에 구독 상품 구매에 대한 처리를 추가합니다.
         self.hideLoadingIndicator()
         print("채은29")
-
+        
         showPaymentConfirmView(state: .yelloPlus)
         print("구독 상품 구매 완료")
         // 서버와 통신 등의 작업을 수행합니다.
@@ -248,21 +254,20 @@ extension PaymentPlusViewController {
     // 원하는 순서대로 제품을 정렬하는 함수입니다.
     private func sortProducts(_ products: [SKProduct]) -> [SKProduct] {
         var sortedProducts: [SKProduct] = []
-
+        
         for productID in productOrder {
             if let product = products.first(where: { $0.productIdentifier == productID }) {
                 sortedProducts.append(product)
             }
         }
-
         return sortedProducts
     }
     
     private func showLoadingIndicator() {
         print("채은30")
-
+        
         navigationController?.interactivePopGestureRecognizer?.isEnabled = false
-
+        
         dimView.backgroundColor = UIColor.black.withAlphaComponent(0.5) // 어둡게 딤처리된 배경색 설정
         
         loadingIndicator.startAnimating()
@@ -275,12 +280,12 @@ extension PaymentPlusViewController {
             $0.center.equalToSuperview()
         }
     }
-
+    
     @objc private func hideLoadingIndicator() {
         print("채은31")
-
+        
         navigationController?.interactivePopGestureRecognizer?.isEnabled = true
-
+        
         loadingIndicator.stopAnimating()
         loadingIndicator.removeFromSuperview()
         dimView.removeFromSuperview()
