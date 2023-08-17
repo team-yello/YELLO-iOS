7847362e4e43b3c328441d831b408df96d58b01e
정채은 <cce523@hanmail.net>
2023-08-18 01:33:21 +0900

    Chore [#182] 결제 service 주석 추가

--- YELLO-iOS/YELLO-iOS/Network/Purchase/Service/IAPService.swift
+++ YELLO-iOS/YELLO-iOS/Network/Purchase/Service/IAPService.swift
@@ -23,26 +23,33 @@ protocol IAPServiceType {
 }
 
 final class IAPService: NSObject, IAPServiceType {
+    
+    // App Store Connect에 등록된 productsID
     private let productIDs: Set<String>
+    // App Stroe Connect에 등록된 product 중 구매한 productsID
     private var purchasedProductIDs: Set<String>
     private var productsRequest: SKProductsRequest?
     private var productsCompletion: ProductsRequestCompletion?
     
+    // 구매 가능 여부
     var canMakePayments: Bool {
         SKPaymentQueue.canMakePayments()
     }
     
     init(productIDs: Set<String>) {
         self.productIDs = productIDs
-        //      self.purchasedProductIDs = productIDs
-        //          .filter { UserDefaults.standard.bool(forKey: $0) == true }
-        self.purchasedProductIDs = Set<String>()
+        self.purchasedProductIDs = productIDs
+            .filter { UserDefaults.standard.bool(forKey: $0) == true }
+        //        self.purchasedProductIDs = Set<String>()
         
         super.init()
+        
+        // App Store Connect와 동기화
         SKPaymentQueue.default().add(self)
         print("채은1")
     }
     
+    // App Store Connect가 갖고 있는 products 조회
     func getProducts(completion: @escaping ProductsRequestCompletion) {
         self.productsRequest?.cancel()
         self.productsCompletion = completion
@@ -52,47 +59,49 @@ final class IAPService: NSObject, IAPServiceType {
         print("채은2")
     }
     
+    // 구매
     func buyProduct(_ product: SKProduct) {
         SKPaymentQueue.default().add(SKPayment(product: product))
         print("채은3")
     }
     
+    // 구매 여부 확인
     func isProductPurchased(_ productID: String) -> Bool {
         self.purchasedProductIDs.contains(productID)
     }
     
+    // 복원
     func restorePurchases() {
         SKPaymentQueue.default().restoreCompletedTransactions()
         print("채은5")
-        
     }
 }
 
+// 앱스토어에 보낸 상품 리스트 요청에 대한 응답을 받는 delegate
 extension IAPService: SKProductsRequestDelegate {
-    // didReceive
+    
+    // 인앱결제 상품 리스트 로드 완료
     func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
         let products = response.products
         self.productsCompletion?(true, products)
         self.clearRequestAndHandler()
         print("채은6")
-        
         products.forEach { print("Found product: \($0.productIdentifier) \($0.localizedTitle) \($0.price.floatValue)") }
     }
     
-    // failed
+    // 인앱결제 상품 리스트 로드 실패
     func request(_ request: SKRequest, didFailWithError error: Error) {
         print("Erorr: \(error.localizedDescription)")
         self.productsCompletion?(false, nil)
         self.clearRequestAndHandler()
         print("채은7")
-        
     }
     
+    // 핸들러 초기화
     private func clearRequestAndHandler() {
         self.productsRequest = nil
         self.productsCompletion = nil
         print("채은8")
-        
     }
 }
 
@@ -101,13 +110,14 @@ extension IAPService: SKPaymentTransactionObserver {
         transactions.forEach {
             switch $0.transactionState {
             case .purchased:
+                // 구입 성공
                 let productID = $0.payment.productIdentifier
                 self.deliverPurchaseNotificationFor(id: productID, transaction: $0)
                 print("completed transaction")
                 SKPaymentQueue.default().finishTransaction($0)
                 print("채은888888")
-                
             case .failed:
+                // 구입 실패
                 if let transactionError = $0.error as NSError?,
                    let description = $0.error?.localizedDescription,
                    transactionError.code != SKError.paymentCancelled.rawValue {
@@ -116,21 +126,18 @@ extension IAPService: SKPaymentTransactionObserver {
                 }
                 SKPaymentQueue.default().finishTransaction($0)
                 NotificationCenter.default.post(name: Notification.Name("HideLoadingIndicator"), object: nil)
-                
             case .restored:
+                // 복원 성공
                 print("failed transaction")
                 self.deliverPurchaseNotificationFor(id: $0.original?.payment.productIdentifier, transaction: $0)
                 SKPaymentQueue.default().finishTransaction($0)
                 print("채은10")
-                
             case .deferred:
                 print("deferred")
                 print("채은11")
-                
             case .purchasing:
                 print("purchasing")
                 print("채은12")
-                
             default:
                 print("unknown")
                 print("채은13")
