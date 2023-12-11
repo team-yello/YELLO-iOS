a70eb6cdb75ad5add768b411619ebbdedda3f268
Zoe <Zoe.urssu@gmail.com>
2023-12-11 15:54:26 +0900

    Fix [#284] 코드리뷰 반영

--- YELLO-iOS/YELLO-iOS/Network/Base/YelloRequestInterceptor.swift
+++ YELLO-iOS/YELLO-iOS/Network/Base/YelloRequestInterceptor.swift
@@ -5,10 +5,9 @@
 //  Created by 지희의 MAC on 2023/07/19.
 //
 
-import Foundation
+import UIKit
 
 import Alamofire
-import UIKit
 import KakaoSDKUser
 
 final class YelloRequestInterceptor: RequestInterceptor {
