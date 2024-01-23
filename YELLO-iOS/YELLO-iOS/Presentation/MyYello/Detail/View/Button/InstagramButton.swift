9e021cf8d982752d92dfcfe89fc05dd25e9fa8f8
chaentopia <cce523@hanmail.net>
2024-01-23 19:42:50 +0900

    Feat [#294] 인스타그램 UI 수정
    하단 버튼들 레이아웃 수정 예정

--- YELLO-iOS/YELLO-iOS/Presentation/MyYello/Detail/View/Button/InstagramButton.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/MyYello/Detail/View/Button/InstagramButton.swift
@@ -8,18 +8,63 @@
 import UIKit
 
 final class InstagramButton: UIButton {
+    
+    let instagramImageView = UIImageView()
+    let instagramLabel = UILabel()
 
     // MARK: - Functions
-    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
-        let expandedBounds = bounds.insetBy(dx: -89.adjustedWidth, dy: -84.adjustedHeight)
-        return expandedBounds.contains(point)
-    }
+//    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+//        let expandedBounds = bounds.insetBy(dx: -89.adjustedWidth, dy: -84.adjustedHeight)
+//        return expandedBounds.contains(point)
+//    }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUI()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUI() {
+        setStyle()
+        setLayout()
+    }
+    
+    func setStyle() {
+        self.backgroundColor = .btnwhite
+        self.makeCornerRound(radius: 20.adjustedHeight)
+        
+        instagramImageView.do {
+            $0.image = ImageLiterals.MyYello.imgInstagram
+        }
+        
+        instagramLabel.do {
+            $0.text = StringLiterals.MyYello.Detail.instagram
+            $0.font = .uiButton
+            $0.textColor = .black
+        }
+    }
+    
+    func setLayout() {
+        self.addSubviews(instagramImageView,
+                         instagramLabel)
+        
+        self.snp.makeConstraints {
+            $0.height.equalTo(40.adjustedHeight)
+            $0.width.equalTo(124.adjustedWidth)
+        }
+        
+        instagramImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(20.adjustedWidth)
+            $0.width.height.equalTo(20.adjusted)
+        }
+        
+        instagramLabel.snp.makeConstraints {
+            $0.leading.equalTo(instagramImageView.snp.trailing).offset(4.adjustedWidth)
+            $0.centerY.equalToSuperview()
+        }
     }
 }
