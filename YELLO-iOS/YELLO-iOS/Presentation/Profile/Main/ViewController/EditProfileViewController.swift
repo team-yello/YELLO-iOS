027a4bace522f2e912ca8d37cc3d17113b1f05a7
Zoe <mam07065@naver.com>
2024-01-25 02:08:50 +0900

    [Add/#293] 프로필 변경 ViewController

---
+++ YELLO-iOS/YELLO-iOS/Presentation/Profile/Main/ViewController/EditProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EditProfileViewController.swift
+//  YELLO-iOS
+//
+//  Created by 지희의 MAC on 1/25/24.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
