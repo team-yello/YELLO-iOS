240ca2c3927d6af1bef212e41ed60e8cf776993f
Zoe <mam07065@naver.com>
2024-01-25 02:22:12 +0900

    [Add/#293] ProfileEditView

---
+++ YELLO-iOS/YELLO-iOS/Presentation/Profile/Main/View/EditProfileView.swift
@@ -0,0 +1,34 @@
+//
+//  EditProfileView.swift
+//  YELLO-iOS
+//
+//  Created by 지희의 MAC on 1/25/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class EditProfileView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func setUI(){
+        setStlye()
+        setLayout()
+    }
+    
+    private func setStlye() {
+    }
+    
+    private func setLayout() {
+        <#code#>
+    }
+}
