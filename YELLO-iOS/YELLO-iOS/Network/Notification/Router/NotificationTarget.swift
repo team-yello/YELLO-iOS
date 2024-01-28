518704e68d5b39114f54fd63cb82c0629191f9d0
heejoo <heejoo_byun@naver.com>
2024-01-28 22:53:21 +0900

    [Feat] #298 - Notification 관련 네트워크 세팅

---
+++ YELLO-iOS/YELLO-iOS/Network/Notification/Router/NotificationTarget.swift
@@ -0,0 +1,51 @@
+//
+//  UserNotificationTarget.swift
+//  YELLO-iOS
+//
+//  Created by 변희주 on 1/28/24.
+//
+
+import Foundation
+
+import Alamofire
+
+enum NotificationTarget {
+    case userNotification
+}
+
+extension NotificationTarget: TargetType {
+    var authorization: Authorization {
+        switch self {
+        case .userNotification:
+            return .authorization
+        }
+    }
+    
+    var headerType: HTTPHeaderType {
+        switch self {
+        case .userNotification:
+            return .plain
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .userNotification:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .userNotification:
+            return "/user/subscribe"
+        }
+    }
+    
+    var parameters: RequestParams {
+        switch self {
+        case .userNotification:
+            return .requestPlain
+        }
+    }
+}
