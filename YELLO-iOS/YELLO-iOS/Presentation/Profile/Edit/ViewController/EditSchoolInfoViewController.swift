d3a47e77e0fc93872321fa74f7d3ff0f913906cd
Zoe <Zoe.urssu@gmail.com>
2024-01-30 22:26:47 +0900

    Feat [#293] 학력 정보 변경 뷰

--- YELLO-iOS/YELLO-iOS/Presentation/Profile/Edit/ViewController/EditSchoolInfoViewController.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Profile/Edit/ViewController/EditSchoolInfoViewController.swift
@@ -7,3 +7,96 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
+class EditSchoolInfoViewController: BaseViewController {
+    let editSchoolInfoView = EditSchoolInfoView()
+    let studentIdViewController = StudentIdViewController()
+    lazy var studentIdView = StudentIdView()
+    let bottomSheetViewController = BaseBottomViewController()
+    
+    override func loadView() {
+        self.view = editSchoolInfoView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDelegate()
+    }
+    
+    private func setDelegate() {
+        editSchoolInfoView.editTableView.dataSource = self
+        editSchoolInfoView.editTableView.delegate = self
+        editSchoolInfoView.navigationBarView.handleBackButtonDelegate = self
+    }
+}
+
+extension EditSchoolInfoViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: EditSchoolInfoTableViewCell.reuseId, for: indexPath) as? EditSchoolInfoTableViewCell {
+            switch indexPath.row {
+            case 0:
+                cell.configureCell(iconType: .search,
+                                   titleText: StringLiterals.Profile.EditProfile.school,
+                                   InfoText: "")
+            case 1:
+                cell.configureCell(iconType: .search,
+                                   titleText: StringLiterals.Profile.EditProfile.major,
+                                   InfoText: "")
+            case 2:
+                cell.configureCell(iconType: .toggle,
+                                   titleText: StringLiterals.Profile.EditProfile.studentId,
+                                   InfoText: "")
+            default:
+                break
+            }
+            cell.selectionStyle = .none
+            return cell
+        }
+        return UITableViewCell()
+    }
+}
+
+extension EditSchoolInfoViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            self.present(FindSchoolViewController(), animated: true)
+        case 1:
+            self.present(FindMajorViewController(), animated: true)
+        case 2:
+            if #available(iOS 16.0, *) {
+                let nav = UINavigationController(rootViewController: studentIdViewController)
+                if let sheet = nav.sheetPresentationController {
+                    sheet.detents = [.medium()]
+                    sheet.prefersGrabberVisible = true
+                    sheet.invalidateDetents()
+                    present(nav, animated: true, completion: nil)
+                }
+            } else {
+                bottomSheetViewController.setCustomView(view: studentIdView)
+                bottomSheetViewController.modalPresentationStyle = .overFullScreen
+                present(bottomSheetViewController, animated: false)
+            }
+            
+        default:
+            break
+        }
+    }
+}
+
+extension EditSchoolInfoViewController: HandleBackButtonDelegate {
+    func popView() {
+        navigationController?.popViewController(animated: true)
+    }
+}
