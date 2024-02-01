8a40bcd885d5eb518f951fc8d7961fd30b7851cd
Zoe <Zoe.urssu@gmail.com>
2024-02-02 07:09:34 +0900

    Add [#315] String Parser 추가

--- YELLO-iOS/YELLO-iOS/Presentation/Profile/Helper/DateConverter.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Profile/Helper/DateConverter.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class DateConverter {
+    static func convertDateString(_ dateString: String) -> String? {
+        
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "yyyy-MM-dd"
+
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateFormat = "yyyy.MM.dd"
+
+        if let date = inputFormatter.date(from: dateString) {
+            let formattedDate = outputFormatter.string(from: date)
+            return formattedDate
+        } else {
+            return nil
+        }
+    }
+}
