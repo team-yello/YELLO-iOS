3cc9bb0815de6892920990b23b1c7f53eebfac33
Zoe <Zoe.urssu@gmail.com>
2024-02-06 23:04:51 +0900

    Fix [#319] 라이팅 변경 적용

--- YELLO-iOS/YELLO-iOS/Global/Literals/String.swift
+++ YELLO-iOS/YELLO-iOS/Global/Literals/String.swift
@@ -362,7 +362,7 @@ enum StringLiterals {
         }
         
         enum Setting {
-            static let setting = "프로필 관리"
+            static let setting = "설정"
             static let center = "고객센터"
             static let privacy = "개인정보 처리방침"
             static let service = "이용약관"
