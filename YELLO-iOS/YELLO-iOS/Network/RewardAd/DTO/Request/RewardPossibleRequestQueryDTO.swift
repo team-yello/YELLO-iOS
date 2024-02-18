637af5960daa36ef058a9226ca0486a92a8d1dac
Zoe <mam07065@naver.com>
2024-02-18 18:44:10 +0900

    Add [#334] 네트워크 관련 함수

---
+++ YELLO-iOS/YELLO-iOS/Network/RewardAd/DTO/Request/RewardPossibleRequestQueryDTO.swift
@@ -0,0 +1,12 @@
+//
+//  RewardPossibleRequestQueryDTO.swift
+//  YELLO-iOS
+//
+//  Created by 지희의 MAC on 2/18/24.
+//
+
+import Foundation
+
+struct RewardPossibleRequestQueryDTO: Codable {
+    let tag: String
+}
---
+++ YELLO-iOS/YELLO-iOS/Network/RewardAd/Router/RewardTarget.swift
@@ -0,0 +1,8 @@
+//
+//  RewardTarget.swift
+//  YELLO-iOS
+//
+//  Created by 지희의 MAC on 2/18/24.
+//
+
+import Foundation
