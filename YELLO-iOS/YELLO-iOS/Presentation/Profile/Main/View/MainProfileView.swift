768a973abb5b8b70a994c1c8d43776ec120513bd
Zoe <Zoe.urssu@gmail.com>
2024-02-21 13:01:20 +0900

    Fix [#340] 레이아웃 이슈 해결

--- YELLO-iOS/YELLO-iOS/Presentation/Profile/Main/View/MainProfileView.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Profile/Main/View/MainProfileView.swift
@@ -24,6 +24,9 @@ final class MainProfileView: UIView {
     let instagramLabel = UILabel()
     let schoolLabel = UILabel()
     
+    let nameInstagramStackView = UIStackView()
+    let userInfoStackView = UIStackView()
+    
     let nameSkeletonLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 66.adjustedWidth, height: 16.adjustedHeight))
     let schoolSkeletonLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 66.adjustedWidth, height: 16.adjustedHeight))
     
@@ -79,7 +82,7 @@ final class MainProfileView: UIView {
         }
         
         nameLabel.do {
-            $0.setTextWithLineHeight(text: " ", lineHeight: 30.adjustedHeight)
+            $0.setTextWithLineHeight(text: "", lineHeight: 28.adjustedHeight)
             $0.font = .uiHeadline02
             $0.textColor = .white
             $0.numberOfLines = 2
@@ -87,7 +90,7 @@ final class MainProfileView: UIView {
         }
         
         instagramLabel.do {
-            $0.setTextWithLineHeight(text: " ", lineHeight: 20.adjustedHeight)
+            $0.setTextWithLineHeight(text: " ", lineHeight: 16.adjustedHeight)
             $0.font = .uiBody02
             $0.textColor = .yelloMain500
         }
@@ -96,8 +99,22 @@ final class MainProfileView: UIView {
             $0.setTextWithLineHeight(text: " ", lineHeight: 16.adjustedHeight)
             $0.font = .uiLabelLarge
             $0.textColor = .grayscales400
+            $0.numberOfLines = 2
             $0.textAlignment = .left
             $0.lineBreakMode = .byCharWrapping
+        }
+        
+        nameInstagramStackView.do {
+            $0.axis = .horizontal
+            $0.addArrangedSubviews(nameLabel,
+                                   instagramLabel)
+        }
+        
+        userInfoStackView.do {
+            $0.axis = .vertical
+            $0.alignment = .leading
+            $0.addArrangedSubviews(nameInstagramStackView,
+                                   schoolLabel)
         }
         
         nameSkeletonLabel.do {
@@ -118,32 +135,25 @@ final class MainProfileView: UIView {
     private func setLayout() {
         self.addSubviews(profileImageView,
                          profileStarImageView,
-                         nameLabel,
-                         instagramLabel,
-                         schoolLabel,
+                         userInfoStackView,
                          editProfileButton,
                          profileStarGradientView,
                          nameSkeletonLabel,
                          schoolSkeletonLabel)
         
         profileImageView.snp.makeConstraints {
-            $0.top.leading.equalToSuperview().offset(20.adjusted)
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(20.adjusted)
             $0.size.equalTo(48.adjusted)
         }
         
-        nameLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(22.adjustedHeight)
+        userInfoStackView.snp.makeConstraints {
             $0.leading.equalTo(profileImageView.snp.trailing).offset(12.adjusted)
-        }
-        
-        instagramLabel.snp.makeConstraints {
-            $0.centerY.equalTo(nameLabel.snp.centerY)
-            $0.leading.equalTo(nameLabel.snp.trailing).offset(10)
+            $0.centerY.equalToSuperview()
         }
         
         schoolLabel.snp.makeConstraints {
-            $0.top.equalTo(nameLabel.snp.bottom)
-            $0.leading.equalTo(nameLabel.snp.leading)
+            $0.width.equalTo(208.adjustedWidth)
         }
         
         editProfileButton.snp.makeConstraints {
