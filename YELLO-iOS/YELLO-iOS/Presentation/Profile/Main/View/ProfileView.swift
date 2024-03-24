1ca7db3de29a5d0860e9a0e56d1f52406b446cb0
정채은 <cce523@hanmail.net>
2024-03-25 00:01:42 +0900

    Fix [#362] 내 프로필 친구 empty 분기처리

--- YELLO-iOS/YELLO-iOS/Presentation/Profile/Main/View/ProfileView.swift
+++ YELLO-iOS/YELLO-iOS/Presentation/Profile/Main/View/ProfileView.swift
@@ -325,10 +325,12 @@ extension ProfileView: UITableViewDataSource {
                 view?.friendCountView.skeletonLabel.isHidden = false
                 view?.friendCountView.skeletonLabel.animateShimmer()
                 view?.friendCountView.countStackView.isHidden = true
+                emptyLabel.isHidden = true
             } else {
                 view?.friendCountView.skeletonLabel.stopShimmering()
                 view?.friendCountView.skeletonLabel.isHidden = true
                 view?.friendCountView.countStackView.isHidden = false
+                emptyLabel.isHidden = friendCount == 0 ? false : true
             }
             
             DispatchQueue.main.async { [self] in
