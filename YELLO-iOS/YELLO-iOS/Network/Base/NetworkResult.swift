bba3139291818755f046799e88a65c2046e5900a
Zoe <mam07065@naver.com>
2024-07-20 00:25:46 +0900

    [Fix/#375] 상태 코드 받도록 변경

--- YELLO-iOS/YELLO-iOS/Network/Base/NetworkResult.swift
+++ YELLO-iOS/YELLO-iOS/Network/Base/NetworkResult.swift
@@ -9,9 +9,9 @@ import Foundation
 
 enum NetworkResult<T> {
     case success(T)     // 서버 통신 성공
-    case requestErr(T)  // 요청에러 발생
+    case requestErr(statusCode: Int)  // 요청에러 발생
     case pathErr // 경로 에러
-    case serverErr  // 서버 내부 에러
+    case serverErr(statusCode: Int)  // 서버 내부 에러
     case networkErr // 네트워크 연결 실패
     case failure // 실패
 }
