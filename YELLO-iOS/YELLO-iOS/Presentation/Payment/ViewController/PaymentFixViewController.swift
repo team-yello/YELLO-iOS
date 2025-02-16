3ece7020dd84e0bfd3833d68559cb552d9a4bcbb
정채은 <cce523@hanmail.net>
2025-02-16 15:48:49 +0900

    Add [#377] fixVC 추가

---
+++ YELLO-iOS/YELLO-iOS/Presentation/Payment/ViewController/PaymentFixViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PaymentFixViewController.swift
+//  YELLO-iOS
+//
+//  Created by 정채은 on 2/16/25.
+//
+
+import UIKit
+
+class PaymentFixViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
